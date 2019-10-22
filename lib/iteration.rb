5107dd7ae513c4d1d857e35c01bce372cd6b681f
Warren McDonald <warrenjmac@mail.com>
2019-10-22 00:56:41 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -44,8 +44,8 @@ def total_even_pairs(src)
   total = 0 
   i = 0 
   while i < src.length do 
-    if src[i][0] %2 === 0 && src[i][1] % 2 === 0
-      total += src[i][0] + src[i][1]
+    if (src[i][0] %2 === 0) && (src[i][1] % 2 === 0)
+      total += (src[i][0] + src[i][1])
     end 
   end 
     total
